1cda8170a9a719752fb9d87b246396bfd58755fe
Shankha Shubhra Das <ribhumaster@gmail.com>
2024-12-06 20:54:17 +0530

    playing with routing and request handlers

--- src/app.js
+++ src/app.js
@@ -2,16 +2,24 @@ const express = require("express");
 
 const app = express();
 
-app.use("/", (req, res) => {
-  res.send("Hello from the dashboard");
-});
-
-app.use("/hello", (req, res) => {
-  res.send("Hello from hello");
-});
-
+// This will match all the HTTP method API calls to /test
 app.use("/test", (req, res) => {
   res.send("Hello from the server");
+});
+
+// This will only handle POST calls to /user
+app.post("/user", (req, res) => {
+  console.log("Saved data to the database");
+  res.send("Data successfully saved to the database");
+});
+
+// This will only handle GET calls to /user
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Shankha", lastName: "Das" });
+});
+
+app.delete("/user", (req, res) => {
+  res.send("Deleted successfully");
 });
 
 app.listen(3000, () => {
